60a8d447e5ebd444ce5ba7022a97b4950b369bc3
David Barron <david.barron91@gmail.com>
2019-04-26 15:45:49 -0500

    issue-1: add dropzone to upload component

--- src/upload/Upload.js
+++ src/upload/Upload.js
@@ -6,6 +6,22 @@ import './Upload.css'
 class Upload extends Component {
     constructor(props) {
       super(props)
+      this.state = {
+        files: [],
+        uploading: false,
+        uploadProgress: {},
+        successfullUploaded: false
+      };
+      this.onFilesAdded = this.onFilesAdded.bind(this);
+      this.uploadFiles = this.uploadFiles.bind(this);
+      this.sendRequest = this.sendRequest.bind(this);
+      this.renderActions = this.renderActions.bind(this);
+    }
+
+    onFilesAdded(files) {
+      this.setState(prevState => ({
+        files: prevState.files.concat(files)
+      }));
     }
   
     render() {
@@ -13,8 +29,23 @@ class Upload extends Component {
           <div className="Upload">
             <span className="Title">Upload Files</span>
             <div className="Content">
-              <div />
-              <div className="Files" />
+              <div>
+                <Dropzone
+                onFilesAdded={this.onFilesAdded}
+                disabled={this.state.uploading || this.state.successfullUploaded}
+                />
+              </div>
+              <div className="Files">
+                {this.state.files.map(file => {
+                  return (
+                    <div key={file.name} className="Row">
+                      <span className="Filename">{file.name}</span>
+                      {this.renderProgress(file)}
+                    </div>
+                  );
+                  })
+                }
+              </div>
             </div>
             <div className="Actions" />
           </div>
